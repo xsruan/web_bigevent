cdb7f67d22a60859e13f2a8bedd4ce0f97820430
RT <ruanxiaosong163@163.com>
2022-06-23 11:03:16 +0800

    提交成功

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,31 @@
+// 注意：每次调用$.get()  $.post()  $.ajax()的时候，
+// 会先调用ajaxPrefilter这个函数
+// 在这个函数中可以拿到我们给Ajax提供的配置对象
+$.ajaxPrefilter(function (options) {
+    // 在发起真正的Ajax请求之前，同意拼接请求的根路径
+    options.url = 'http://www.liulongbin.top:3007' + options.url
+    console.log(options.url)
+
+
+    // 统一为有权限的接口，设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局同意挂载complete回调函数
+    // 不论成功或者失败，最终都会调用complete回调函数
+    options.complete = function (res) {
+        // console.log('执行了 complete回调函数');
+        // console.log(res);
+        // 在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.强制清空token
+            localStorage.removeItem('token')
+            // 2.强制跳转到登录页
+            location.href = '/login.html'
+        }
+
+    }
+})
